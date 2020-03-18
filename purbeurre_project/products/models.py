047390ea40d13c9b3f4a1120022b568a85d0b9e8
finevine <finevine@gmail.com>
2020-03-18 14:42:50 +0100

    first model for product

--- purbeurre_project/products/models.py
+++ purbeurre_project/products/models.py
@@ -1,16 +1,39 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 
 class Product(models.Model):
-    nutrition_grade = models.CharField(max_length=1)
-    name = models.CharField(max_length=200)
     code = models.CharField(max_length=30)
-    category = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
+    nutritionGrade = models.CharField(max_length=1)
+    image = models.URLField()
 
     def __str__(self):
-        return self.question_text
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
 
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+class Nutition100(models.Model):
+    code = models.OneToOneField(
+        Product,
+        on_delete=models.CASCADE,
+        primary_key=True,
+    )
+    fat = models.DecimalField("Fat in 100g", max_digits=5, decimal_places=2)
+    satFat = models.DecimalField("Saturated fat in 100g", max_digits=5, decimal_places=2)
+    sugar = models.DecimalField("Sugar in 100g", max_digits=5, decimal_places=2)
+    salt = models.DecimalField("Salt in 100g", max_digits=5, decimal_places=2)
+
+class Category(models.Model):
+    code = models.ForeignKey(Product, on_delete=models.CASCADE)
+    name = models.CharField("Category name", max_length=100, primary_key=True)
+
+class Favourite(models.Model):
+    codeHealthy = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="healthy")
+    codeUnhealthy = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="unhealthy")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    # user = models.ForeignKey()
---
+++ purbeurre_project/products/tests/test_urls.py
@@ -0,0 +1,4 @@
+from django.test import TestCase
+
+# Create your tests here.
+# https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2
