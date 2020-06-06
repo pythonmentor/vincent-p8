299a5dd372eb9eb813307ec154af005b25ecd9a1
finevine <finevine@gmail.com>
2020-06-06 20:58:12 +0200

    Heroku logging

--- purbeurre_project/purbeurre_project/settings.py
+++ purbeurre_project/purbeurre_project/settings.py
@@ -151,6 +151,22 @@ if os.environ.get('ENV') == 'PRODUCTION':
     # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
+    # LOGGING For Heroku 
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+            },
+        },
+    }
     # Activate Django-Heroku.
     django_heroku.settings(locals())
 else:
