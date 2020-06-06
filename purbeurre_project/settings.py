471b850e1d29ebecfc729b9f67d8192b55eae4f8
finevine <finevine@gmail.com>
2020-06-06 22:37:22 +0200

    STATIC_DIRS change

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -134,7 +134,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
     STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
     # Extra places for collectstatic to find static files.
     STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
+        os.path.join(BASE_DIR, 'templates/static'),
     )
 
     # Simplified static file serving.
