403f4850eecb555f980d86370d8bfbb870615451
Thierry Chappuis <thierry@chappuis.io>
2020-06-07 12:00:31 +0200

    static back in templates

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -142,7 +142,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static/dist'),
+    os.path.join(BASE_DIR, 'templates/static/dist'),
     os.path.join(BASE_DIR, "static"),
 )
 
