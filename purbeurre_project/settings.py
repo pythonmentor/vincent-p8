7f89ec73b2dbf8135195c9c910aa9a51c3bda2b6
Thierry Chappuis <thierry@chappuis.io>
2020-06-07 12:08:21 +0200

    static out of templates

--- purbeurre_project/settings.py
+++ purbeurre_project/settings.py
@@ -141,7 +141,7 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'templates/static/dist'),)
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static/dist'),)
 
 # LOGGING For Heroku
 LOGGING = {
